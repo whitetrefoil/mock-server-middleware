2b9e01e67ae478b4e30acc466090b82d38ede76d
WhiteTrefoil <whitetrefoil@gmail.com>
2018-07-16 15:01:48 +0800

    Fix wrong entry file name

--- tests/basic_spec.ts
+++ tests/basic_spec.ts
@@ -1,8 +1,8 @@
 import { expect } from 'chai'
+import * as msm from '../src/main'
 
 describe('Basic', () => {
   it('should exports correctly', () => {
-    const msm = require('../src')
     expect(msm).to.be.a('object')
     expect(msm.MSM).to.be.a('function')
     expect(msm.MSM.name).to.equal('MSM')
